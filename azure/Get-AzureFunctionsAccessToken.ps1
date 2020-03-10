c149ea287a40308b1b1024875c26fffb93e7a5ad
Martin Brandl <mjibrandl@gmail.com>
2020-03-10 10:49:14 +0100

    added Get-AzureFunctionsAccessToken script

---
+++ azure/Get-AzureFunctionsAccessToken.ps1
@@ -0,0 +1,28 @@
+﻿param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ResourceGroupName,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    $FunctionAppName
+)
+
+$publishingCredentials = Invoke-AzResourceAction  `
+    -ResourceGroupName $ResourceGroupName `
+    -ResourceType 'Microsoft.Web/sites/config' `
+    -ResourceName ('{0}/publishingcredentials' -f $FunctionAppName) `
+    -Action list `
+    -ApiVersion 2019-08-01 `
+    -Force
+
+$base64Credentials = [Convert]::ToBase64String(
+    [Text.Encoding]::ASCII.GetBytes(
+        ('{0}:{1}' -f $publishingCredentials.Properties.PublishingUserName, $publishingCredentials.Properties.PublishingPassword)
+    )
+)
+
+Invoke-RestMethod `
+    -Uri ('https://{0}.scm.azurewebsites.net/api/functions/admin/token' -f $FunctionAppName) `
+    -Headers @{ Authorization = ('Basic {0}' -f $base64Credentials) }
+
